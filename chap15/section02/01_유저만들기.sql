0dd4a1482c64872ecbd3c12dced1b0e89ff64ecf
forbob <forbob@naver.com>
2025-05-23 07:41:51 +0900

    1
    1

--- chap15/section02/01_유저만들기.sql
+++ chap15/section02/01_유저만들기.sql
@@ -1,8 +1,4 @@
--- 권한, 롤(역할)
--- 주로 DBA 가 하는 일
--- 1) 유저 만들기
--- Oracle DB 관리자 : system(sys(최고관리자))
--- 예제 1) usertest01 유저 생성
---         패스워드 : pass1 지정
--- 사용법) CREATE USER 계정명 IDENTIFIED BY 패스워드;
-CREATE USER usertest01 IDENTIFIED BY pass1;
+-- 기본 : c##유저명 만들어야하는데 안붙여도 되게하는 명령어 
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+CREATE USER USER01 IDENTIFIED BY 1234567890;
