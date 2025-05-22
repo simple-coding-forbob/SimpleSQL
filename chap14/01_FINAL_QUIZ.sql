2f05740ea6eb774cad9cfba1325d74cebcd1d4da
forbob <forbob@naver.com>
2025-05-23 03:58:19 +0900

    1
    1

--- chap14/01_FINAL_QUIZ.sql
+++ chap14/01_FINAL_QUIZ.sql
@@ -1,16 +1,4 @@
--- 28_제약조건_연습문제.sql
---— 1) EMPLOYEE 테이블의 구조를 복사하여 EMP_SAMPLE 란 
---     이름의 테이블을 만드시오.
---—   사원 테이블의 사원번호 컬럼(ENO)에 
---     테이블 레벨로 PRIMARY KEY 제약조건을 지정하되,
---—   제약 조건 이름은 PK_MY_EMP 하시오.
--- 1-1) 복사본 : 제약조건 복사 안됨(기본키,외래키 직접 생성해야 함)
--- 힌트) 사용법 : CREATE TABLE 테이블명 
---               AS 
---               SELECT * FROM 원본테이블명
---               WHERE 1=2;
--- 사용법) ALTER TABLE 테이블명
---        ADD CONSTRAINT 제약조건명 PRIMARY KEY(컬럼)
+--1)
 CREATE TABLE EMP_SAMPLE 
 AS 
 SELECT * FROM EMPLOYEE
@@ -24,7 +12,7 @@ ADD CONSTRAINT PK_MY_EMP PRIMARY KEY(ENO);
 --—   부서번호 컬럼(DNO)에 PRIMARY KEY 제약조건을 지정하되,
 --—   제약 조건 이름은 PK_MY_DEPT 하시오.
 -- 1-1) 복사본 : 제약조건 복사 안됨(기본키,외래키 직접 생성해야 함)
-CREATE TABLE DEPT_SAMPLE 
+CREATE TABLE DEPT_SAMPLE
 AS 
 SELECT * FROM DEPARTMENT
 WHERE 1=2;
--- chap15/section01/01_테이블예_컬럼_추가하기.sql
+++ chap15/section01/01_테이블예_컬럼_추가하기.sql
@@ -1,16 +1,8 @@
--- 26_테이블_변경.sql
--- (참고) 테이블 설계(구조)를 변경하기 : 컬럼명, 데이터형
--- 실습 테이블 : DEPT20
---      DEPARTMENT 복사
-CREATE TABLE DEPT20
+
+CREATE TABLE DEPT
 AS
 SELECT * FROM DEPARTMENT
-WHERE 1=2;              -- 거짓조건
+WHERE 1=2;              
 
--- 예제 1) 부서 테이블에(DEPT20) 날짜 자료형을(DATE) 가지는 BIRTH 컬럼 추가하기
--- 사용법) ALTER TABLE 테이블명 ADD(컬럼명 자료형);
-ALTER TABLE DEPT20
+ALTER TABLE DEPT
 ADD(BIRTH DATE);
-
--- 구조 보기
-DESC DEPT20;
