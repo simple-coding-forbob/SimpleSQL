e6531f4386223d5e16bdb6484adbdee5e1cb9c15
simple-coding-forbob <96178104+simple-coding-forbob@users.noreply.github.com>
2025-06-04 14:26:09 +0900

    1
    1

--- chap05/section03/05_널변경.sql
+++ chap05/section03/05_널변경.sql
@@ -1,5 +1,5 @@
 -- 일반 함수들 : 1) NVL() : NULL 값 -> 특정 문자로(숫자) 변경하는 함수 : NVL(컬럼명, 바꿀문자[숫자])
--- 연봉 + 보너스 구하기 : SALARY(급여) + COMMISSION(보너스)
 -- 사원 : EMPLOYEE
-SELECT NVL(COMMISSION, 0)
+SELECT COMMISSION, 
+       NVL(COMMISSION, 0)
 FROM EMPLOYEE;
